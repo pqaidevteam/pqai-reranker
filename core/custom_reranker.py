681e4ad045dfa0361ca626f24270e2e723cc917a
Mahesh Maan <mahesh.maan@greyb.com>
2022-06-17 15:57:57 +0530

    Cleanup and refactor code

--- core/custom_reranker.py
+++ core/custom_reranker.py
@@ -1,48 +1,68 @@
-import numpy as np
-from pathlib import Path
-from nltk.tokenize import RegexpTokenizer
-import numba
-import json
-import math
+"""Summary
 
-from core.encoder_srv import encode
+Attributes:
+    BASE_DIR (TYPE): Description
+    embeddings (TYPE): Description
+    MODELS_DIR (TYPE): Description
+    sifs (TYPE): Description
+"""
+import math
+from pathlib import Path
+import json
+import numba
+import numpy as np
+from nltk.tokenize import RegexpTokenizer
+
 from scipy.spatial import distance
+from core.encoder_srv import encode
 from core.reranker import Ranker
 
 BASE_DIR = str(Path(__file__).parent.parent.resolve())
-models_dir = "{}/assets/".format(BASE_DIR)
+MODELS_DIR = "{}/assets/".format(BASE_DIR)
 
 class GloveWordEmbeddings:
+
+    """Glove word embeddings"""
+
     def __init__(self):
-        self.models_dir = models_dir
-        self.vocab_file = self.models_dir + "/glove-vocab.json"
-        self.dict_file = self.models_dir + "/glove-dictionary.json"
-        self.dfs_file = self.models_dir + "/dfs.json"
-        self.embeddings_file = self.models_dir + "/glove-We.npy"
-        self.vocab = None
-        self.dictionary = None
-        self.dfs = None
-        self.sifs = None
-        self.embeddings = None
-        self.dims = None
+        """Initialize"""
+        self._models_dir = MODELS_DIR
+        self._vocab_file = f"{self._models_dir}/glove-vocab.json"
+        self._dict_file = f"{self._models_dir}/glove-dictionary.json"
+        self._dfs_file = f"{self._models_dir}/dfs.json"
+        self._embs_file = f"{self._models_dir}/glove-We.npy"
+        self._vocab = None
+        self._dictionary = None
+        self._dfs = None
+        self._sifs = None
+        self._embs = None
+        self._dims = None
         self._load()
 
     def _load(self):
-        with open(self.vocab_file) as file:
-            self.vocab = json.load(file)
-        with open(self.dict_file) as file:
-            self.dictionary = json.load(file)
-        with open(self.dfs_file) as file:
-            self.dfs = json.load(file)
-        self.embeddings = np.load(self.embeddings_file)
-        self.sifs = {word: self.df2sif(word, self.dfs) for word in self.dfs}
-        self.dims = self.embeddings.shape[1]
-
-    def __len__(self):
-        return self.embeddings.shape[0]
+        """Load the embeddings data from the disk
+        """
+        with open(self._vocab_file) as file:
+            self._vocab = json.load(file)
+        with open(self._dict_file) as file:
+            self._dictionary = json.load(file)
+        with open(self._dfs_file) as file:
+            self._dfs = json.load(file)
+        self._embs = np.load(self._embs_file)
+        self._sifs = {word: self.df2sif(word, self._dfs) for word in self._dfs}
+        self._dims = self._embs.shape[1]
 
     @staticmethod
     def df2sif(word, dfs):
+        """Return SIF (smooth inverse frequency) of a given word
+
+        Args:
+            word (str): Word
+            dfs (dict): Document frequencies of words {"word": df}
+
+        Returns:
+            float: SIF value
+        """
         n = dfs[word]
         N = dfs["the"]
         p = n / N
@@ -50,61 +70,95 @@ class GloveWordEmbeddings:
         w = a / (a + p)
         return w
 
+    def __len__(self):
+        """Return the number of words for which vectors are available
+
+        Returns:
+            int: Word count
+        """
+        return self._embs.shape[0]
+
     def __getitem__(self, item):
+        """Get a word vectors
+
+        Args:
+            item (str): Word
+
+        Returns:
+            List[float]: Word vector
+        """
         if type(item) is int:
-            return self.embeddings[item]
+            return self._embs[item]
         elif type(item) is str:
-            item = item if item in self.dictionary else "<unk>"
-            return self.embeddings[self.dictionary[item]]
+            item = item if item in self._dictionary else "<unk>"
+            return self._embs[self._dictionary[item]]
         else:
-            return np.zeros(self.dims)
+            return np.zeros(self._dims)
 
     def get_sif(self, word):
-        return self.sifs.get(word, 1.0)
+        """Return SIF for a given word
+
+        Args:
+            word (str): Word
+
+        Returns:
+            float: SIF value
+        """
+        return self._sifs.get(word, 1.0)
 
 
 embeddings = GloveWordEmbeddings()
-sifs = embeddings.sifs
-
-class Text(str):
-    def __init__(self, text):
-        self._text = text
-        self._default_tokenizer = RegexpTokenizer(r"\w+")
-
-    def to_tokens(self, tokenizer=None):
-        if not tokenizer:
-            tokenizer = self._default_tokenizer
-        tokens = tokenizer.tokenize(self._text_lower)
-        return TokenSequence(tokens)
-
-    @property
-    def _text_lower(self):
-        return self._text.lower()
-
-    def __repr__(self):
-        prefix = "Text: "
-        if len(self._text) < 77:
-            return prefix + self._text
-        else:
-            return prefix + self._text[:17] + "..."
+sifs = embeddings._sifs
 
 
 class TokenSequence(list):
+
+    """Summary
+    """
+
     def __init__(self, tokens):
+        """Summary
+
+        Args:
+            tokens (TYPE): Description
+        """
         super().__init__(tokens)
         self._tokens = tokens
 
     def to_vector_sequence(self, token_embeddings):
+        """Summary
+
+        Args:
+            token_embeddings (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         vectors = [token_embeddings[token] for token in self._tokens]
         return VectorSequence(self._tokens, vectors)
 
     @property
     def tokens(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         return self._tokens
 
 
 class VectorSequence:
+
+    """Summary
+    """
+
     def __init__(self, labels, vectors):
+        """Summary
+
+        Args:
+            labels (TYPE): Description
+            vectors (TYPE): Description
+        """
         self._labels = labels
         self._sequence = np.array(vectors)
         self._n = len(vectors)
@@ -119,29 +173,65 @@ class VectorSequence:
 
     @property
     def labels(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         return self._labels
 
     def __repr__(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         text = f"VectorSequence: {len(self._labels)} labels, {len(self._sequence)} vectors;"
         text += f' Labels: {", ".join(self._labels[:5])}'
         text += ", ..." if self._labels[5:] else ""
         return text
 
     def _weighted_by_tokens(self, weights):
+        """Summary
+
+        Args:
+            weights (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         W = [weights[token] for token in self._tokens]
         return self.weighted_by_vectors(W)
 
     def _weighted_by_vectors(self, W):
+        """Summary
+
+        Args:
+            W (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         W = np.array(W).reshape(1, -1)
         return self._sequence * W.T
 
     def weigh(self, weights):
+        """Summary
+
+        Args:
+            weights (TYPE): Description
+        """
         if isinstance(weights, dict):
             self._weighted_by_tokens(weights)
         self._weighted_by_vector(weights)
 
     @property
     def redundancy_vector(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         interact = self._default_interaction.interact
         interactions = interact(self, self)
         interactions = np.tril(interactions._matrix, -1)
@@ -149,6 +239,11 @@ class VectorSequence:
 
     @property
     def matrix(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         if self._fixed_length is None:
             return self._sequence
         if self._n > self._fixed_length:
@@ -158,21 +253,44 @@ class VectorSequence:
 
     @property
     def _truncated(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         return self._sequence[: self._fixed_length]
 
     @property
     def _padded(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         r = self._fixed_length - self._n
         shape = (r, self._dims)
         padding = np.zeros(shape)
         return np.concatenate((self._sequence, padding))
 
     def set_length(self, n):
+        """Summary
+
+        Args:
+            n (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         self._fixed_length = n
         return self
 
     @property
     def normalized_matrix(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         row_magnitudes = np.sqrt(
             np.sum(self._sequence * self._sequence, axis=1, keepdims=True)
         )
@@ -181,9 +299,32 @@ class VectorSequence:
 
 
 class Interaction:
-    def __init__(
-        self, metric="cosine", context=False, amplify=False, reinforce=False, window=5
-    ):
+
+    """Summary
+
+    Attributes:
+        amplify (TYPE): Description
+        context (TYPE): Description
+        metric (TYPE): Description
+        reinforce (TYPE): Description
+        window_size (TYPE): Description
+    """
+
+    def __init__(self,
+                 metric="cosine",
+                 context=False,
+                 amplify=False,
+                 reinforce=False,
+                 window=5):
+        """Summary
+
+        Args:
+            metric (str, optional): Description
+            context (bool, optional): Description
+            amplify (bool, optional): Description
+            reinforce (bool, optional): Description
+            window (int, optional): Description
+        """
         self.metric = metric
         self.context = context
         self.amplify = amplify
@@ -197,19 +338,54 @@ class Interaction:
         self._h = 0.0
 
     def _dot_interaction(self, A, B):
+        """Summary
+
+        Args:
+            A (TYPE): Description
+            B (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return np.matmul(A, B.T)
 
     def _cosine_interaction(self, A, B):
+        """Summary
+
+        Args:
+            A (TYPE): Description
+            B (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         An = self._normalize_rows(A)
         Bn = self._normalize_rows(B)
         return self._dot_interaction(An, Bn)
 
     def _euclidean_interaction(self, A, B):
+        """Summary
+
+        Args:
+            A (TYPE): Description
+            B (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         diff = A - B
         sq_diff = diff * diff
         return np.sqrt(sq_diff)
 
     def _context_sequence(self, vector_seq):
+        """Summary
+
+        Args:
+            vector_seq (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         M = vector_seq.matrix
         C = np.zeros(M.shape)
         C *= np.array(
@@ -222,6 +398,15 @@ class Interaction:
         return C
 
     def interact(self, vector_seq_A, vector_seq_B):
+        """Summary
+
+        Args:
+            vector_seq_A (TYPE): Description
+            vector_seq_B (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         A = vector_seq_A.matrix
         B = vector_seq_B.matrix
         I = self.interaction_fn(A, B)
@@ -243,6 +428,11 @@ class Interaction:
 
     @property
     def interaction_fn(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         if self.metric == "cosine":
             return self._cosine_interaction
         elif self.metric == "dot":
@@ -252,36 +442,97 @@ class Interaction:
 
     @staticmethod
     def _normalize_rows(M):
+        """Summary
+
+        Args:
+            M (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         row_magnitudes = np.sqrt(np.sum(M * M, axis=1, keepdims=True))
         row_magnitudes += np.finfo(float).eps
         return M / row_magnitudes
 
     @staticmethod
     def _reinforce(A, B):
+        """Summary
+
+        Args:
+            A (TYPE): Description
+            B (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return 0.25 * (A + B + 2 * (A * B))
 
     def _amplify(self, x):
+        """Summary
+
+        Args:
+            x (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return self._h + (self._f / (1 + (self._a * math.exp(self._b * (x - self._c)))))
 
     @staticmethod
     @numba.vectorize([numba.float64(numba.float64)])
     def _amplifier(x):
+        """Summary
+
+        Args:
+            x (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return 1 / (1 + (3.2 * math.exp(-7.5 * (x - 0.46))))
 
 
 class InteractionMatrix:
+
+    """Summary
+    """
+
     def __init__(self, I):
+        """Summary
+
+        Args:
+            I (TYPE): Description
+        """
         self._matrix = I
 
     def available_metrics(self):
+        """Summary
+
+        Returns:
+            TYPE: Description
+        """
         return self._available_interactions
 
     def maxpool(self, direction="horizontal"):
+        """Summary
+
+        Args:
+            direction (str, optional): Description
+
+        Returns:
+            TYPE: Description
+        """
         axis = 1 if direction == "horizontal" else 0
         return np.max(self._matrix, axis=axis)
 
 class CustomRanker(Ranker):
+
+    """Summary
+    """
+
     def __init__(self):
+        """Summary
+        """
         self._interaction = Interaction()
         self._interaction.metric = "cosine"
         self._interaction.amplify = True
@@ -292,8 +543,17 @@ class CustomRanker(Ranker):
         super().__init__("similarity")
 
     def score(self, query, doc):
-        query_tokens = Text(query).to_tokens()
-        doc_tokens = Text(doc).to_tokens()
+        """Summary
+
+        Args:
+            query (str): Query
+            doc (str): Document
+
+        Returns:
+            float: Similarity score between query and document
+        """
+        query_tokens = TokenSequence(RegexpTokenizer(r"\w+").tokenize(query.lower()))
+        doc_tokens = TokenSequence(RegexpTokenizer(r"\w+").tokenize(doc.lower()))
         nq = len(query_tokens)
         nd = max(1, len(doc_tokens))
         doc_length_surplus = max(1, nd / nq)
@@ -301,7 +561,7 @@ class CustomRanker(Ranker):
         Q = query_tokens.to_vector_sequence(embeddings)
         D = doc_tokens.to_vector_sequence(embeddings)
         query_term_matches = self._interact(Q, D).maxpool()
-        sifs = embeddings.sifs
+        sifs = embeddings._sifs
         query_term_weights = [
             (sifs[word] if word in sifs else 1.0) for word in query_tokens
         ]
